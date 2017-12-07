ab6ce2655cd9614c216a795b711135696e6f56bc
Zelin Su <1005719242@qq.com>
2017-12-07 22:55:11 +0800

    Add files via upload

--- code/calculator_algorithm.py
+++ code/calculator_algorithm.py
@@ -1,15 +1,30 @@
-#!/usr/bin/python
 # -*- coding: UTF-8 -*-
+"""
+author NorwegianForest
+计算器算法
+输入一串运算式，运算符前后由空格隔开
+返回运算结果
+支持加减乘除和括号运算
+"""
+
 from collections import deque
 from decimal import Decimal
 
 
 def single_calculate(num1, num2, operator):
+    """
+    单次运算
+    :param num1: 操作数1
+    :param num2: 操作数2
+    :param operator: 运算符
+    :return: 运算结果
+    """
     # num1的格式为Decimal('num1')
     num1 = Decimal(num1)
     num2 = Decimal(num2)
+    # 用str返回只有数字的字符串
     if operator == "+":
-        return str(num2 + num1)  # 用str返回只有数字的字符串
+        return str(num2 + num1)
     elif operator == "-":
         return str(num2 - num1)
     elif operator == "*":
@@ -21,43 +36,58 @@ def single_calculate(num1, num2, operator):
 
 
 def formula_calculate(formula):
-    formula = formula.split()  # 根据空格分开
-    formula = deque(formula)  # 创建队列
+    """
+    算式运算
+    :param formula: 算式字符串，运算符前后由空格隔开
+    :return: 运算结果
+    """
+    formula = formula.split()
+    formula = deque(formula)
     number_stack = []
     operator_stack = []
 
     while 1:
         if len(formula) != 0:
             unknow = formula.popleft()
-            if unknow == "+" or unknow == "-" or unknow == "*" or unknow == "/":  # 队列输入的是运算符
-                if len(operator_stack) == 0:  # 运算符堆栈为空
+            # 队列输入的是运算符
+            if unknow == "+" or unknow == "-" or unknow == "*" or unknow == "/":
+                # 运算符堆栈为空
+                if len(operator_stack) == 0:
                     operator_stack.append(unknow)
                 # 运算符优先级高于栈顶运算符
                 elif (unknow == "*" or unknow == "/") and (operator_stack[-1] == "+" or operator_stack[-1] == "-"):
                     operator_stack.append(unknow)
-                elif operator_stack[-1] == "(":  # 栈顶为左括号时，相当于运算符堆栈为空，一律入栈
+                # 栈顶为左括号时，相当于运算符堆栈为空，一律入栈
+                elif operator_stack[-1] == "(":
                     operator_stack.append(unknow)
-                else:  # 无需入栈，直接计算
-                    formula.appendleft(unknow)  # 将运算符返回队列，以便下一次循环
+                # 无需入栈，直接计算
+                else:
+                    # 将运算符返回队列，以便下一次循环
+                    formula.appendleft(unknow)
                     number_stack.append(single_calculate(number_stack.pop(), number_stack.pop(), operator_stack.pop()))
                     if number_stack[-1] == "ERROR":
-                        break  # 错误检测
-            elif unknow == "(":  # 左括号一律入栈
+                        break
+            # 左括号一律入栈
+            elif unknow == "(":
                 operator_stack.append(unknow)
             elif unknow == ")":
                 operator_top = operator_stack.pop()
-                if operator_top != "(":  # 运算符栈顶非左括号则进行计算
+                # 运算符栈顶非左括号则进行计算
+                if operator_top != "(":
                     number_stack.append(single_calculate(number_stack.pop(), number_stack.pop(), operator_top))
                     if number_stack[-1] == "ERROR":
                         break
                     formula.appendleft(unknow)
-            else:  # 非符号则为数字
+            # 非符号则为数字
+            else:
                 number_stack.append(unknow)
-        else:  # 队列输入完毕，进行最后一次运算
+        # 队列输入完毕，进行最后一次运算
+        else:
             number_stack.append(single_calculate(number_stack.pop(), number_stack.pop(), operator_stack.pop()))
             if number_stack[-1] == "ERROR":
                 break
-            if len(operator_stack) == 0:  # 运算符堆栈清空，退出循环
+            # 运算符堆栈清空，退出循环
+            if len(operator_stack) == 0:
                 break
     return number_stack.pop()
 
